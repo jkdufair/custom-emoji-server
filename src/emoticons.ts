a8b65327a7a70968e6adf92445f5c3c6f71344c5
Jason Dufair <jase@dufair.org>
2021-09-29 16:03:56 -0400

    Move to redis from postgres

--- src/emoticons.ts
+++ src/emoticons.ts
@@ -1,137 +1,82 @@
 import * as bodyParser from 'body-parser';
 import express, { Router } from 'express';
-import { default as fsWithCallbacks } from 'fs';
-import { Pool } from 'pg';
-const fs = fsWithCallbacks.promises;
+import { createNodeRedisClient as createClient } from 'handy-redis';
+import { promisify } from 'util';
 
 export const emoticonRouter = express.Router();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-});
-
 interface EmoticonMap { [emoticon: string]: string; }
+
 const emoticonMatch = /[^~]+/;
 const emojiListPath = './assets/emoji/';
 
 emoticonRouter.use((req, res, next) => {
-    const origin = res.get('origin') || 'https://teams.microsoft.com';
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+  const origin = res.get('origin') || 'https://teams.microsoft.com';
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
-
-emoticonRouter.post('/init',
-    async (req, res) => {
-        try {
-            const client = await pool.connect();
-            await client.query(
-                'create table if not exists emoji (id integer, name text, image bytea)'
-            )
-            res.status(200).send('DB initialized successfully');
-        } catch (err) {
-            res.status(500).send('Error ' + err);
-        }
-    });
 
 emoticonRouter.post('/emoticon/:emoticon',
-    bodyParser.raw({
-        limit: '1mb',
-        type: 'image/*'
-    }), async (req, res) => {
-        const data: Buffer = req.body;
-        const hexData = '\\x' + data.toString('hex');
-        const emojiName = req.params.emoticon;
-        try {
-            const client = await pool.connect();
-            const emojiIfExists = await client.query(
-                'select name from emoji where name = $1',
-                [emojiName]);
-            if (emojiIfExists && emojiIfExists.rows && emojiIfExists.rows.length > 0) {
-                res.status(400).send('emoji already exists');
-                return;
-            }
-            await client.query(
-                'Insert into emoji (name, image, uses) values ($1, $2, 0)',
-                [emojiName, hexData]);
-            client.release();
-            res.status(200).send('successfully inserted emoji ' + emojiName);
-        } catch (err) {
-            res.status(500).send('Error ' + err);
-        }
-    });
-
-type UsageData = Record<string, number>;
-emoticonRouter.post('/emoticons/usage', bodyParser.json(), async (req, res) => {
-    const usages: UsageData = req.body;
-    if(Object.keys(usages).length <= 0) {
-        res.status(400).send('No usage data found in body: ' + JSON.stringify(usages));
-        return;
-    }
-    try {
-        const client = await pool.connect();
-        
-        var requests = Object.keys(usages).map(emoticon => {
-            return client.query('update emoji set uses = uses + $1 where name = $2', [usages[emoticon], emoticon])
-        });
-        var result = await Promise.all(requests);
-        client.release();
-        res.status(200).send('successfully updated emoticon usages: ' + Object.keys(usages).join(', '));
-    } catch (err) {
-        res.status(500).send('Error ' + err);
-    }
-});
+										bodyParser.raw({
+											limit: '1mb',
+											type: 'image/*'
+										}), async (req, res) => {
+											const data: Buffer = req.body;
+											const hexData = data.toString('base64');
+											const emojiName = req.params.emoticon;
+											try {
+												const client = await createClient();
+												const emojiIfExists = await client.get(emojiName);
+												if (emojiIfExists) {
+													res.status(400).send('emoji already exists');
+													return;
+												}
+												await client.set(emojiName, hexData);
+												res.status(200).send('successfully inserted emoji ' + emojiName);
+											} catch (err) {
+												res.status(500).send('Error ' + err);
+											}
+										});
 
 emoticonRouter.delete('/emoticon/:emoticon', async (req, res) => {
-    const emojiName = req.params.emoticon;
-    try {
-        const client = await pool.connect();
-        await client.query(
-            'delete from emoji where name = $1',
-            [emojiName]);
-        client.release();
-        res.status(200).send('deleted emoji ' + emojiName);
-    } catch (err) {
-        res.status(500).send('Error ' + err);
-    }
+  const emojiName = req.params.emoticon;
+  try {
+    const client = await createClient();
+		client.del(emojiName);
+    res.status(200).send('deleted emoji ' + emojiName);
+  } catch (err) {
+    res.status(500).send('Error ' + err);
+  }
 });
 
 emoticonRouter.get('/emoticon/:emoticon', async (req, res) => {
-    const emoticonName = req.params.emoticon;
+  const emoticonName = req.params.emoticon;
 
-    let imageData: Buffer;
-    try {
-        const client = await pool.connect();
-        const result = await client.query(
-            'select image from emoji where name = $1',
-            [emoticonName]);
-        client.release();
-        if (!result || !result.rows || result.rows.length === 0) {
-            res.status(404).send('not found');
-            return;
-        }
-        const rows = result.rows;
-        imageData = rows[0].image;
-    } catch (err) {
-        res.status(500).send('Error ' + err);
-        return;
+	let imageData;
+  try {
+    const client = await createClient();
+    imageData = await client.get(emoticonName);
+    if (!imageData) {
+      res.status(404).send('not found');
+      return;
     }
+  } catch (err) {
+    res.status(500).send('Error ' + err);
+    return;
+  }
 
-    res.set('Content-Type', 'image/gif');
-    const maxAge = Math.round(60 * 60 * 24 * (15 + (Math.random() * 2 - 1)));
-    res.header('Cache-Control', `public, max-age=${maxAge}`);
-    res.send(imageData);
+  res.set('Content-Type', 'image/gif');
+  const maxAge = Math.round(60 * 60 * 24 * (15 + (Math.random() * 2 - 1)));
+  res.header('Cache-Control', `public, max-age=${maxAge}`);
+  res.send(Buffer.from(imageData, 'base64'));
 });
 
 emoticonRouter.get('/emoticons', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('select name from emoji');
-        client.release();
-        const rows: Array<{ name: string }> = result.rows;
-        res.send(rows.map(row => row.name));
-    } catch (err) {
-        res.status(500).send('Error ' + err);
-    }
+  try {
+    const client = await createClient();
+		res.send(await client.keys('*'));
+  } catch (err) {
+    res.status(500).send('Error ' + err);
+  }
 });
