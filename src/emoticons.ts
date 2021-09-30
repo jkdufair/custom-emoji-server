32651f11a2350eea59f3e89b9e2ab68fd6d73e7e
Jason Dufair <jase@dufair.org>
2021-09-30 10:00:45 -0400

    Use hex strings. Add connection string. Cleanup.

--- src/emoticons.ts
+++ src/emoticons.ts
@@ -4,10 +4,7 @@ import { createNodeRedisClient as createClient } from 'handy-redis';
 
 export const emoticonRouter = express.Router();
 
-interface EmoticonMap { [emoticon: string]: string; }
-
-const emoticonMatch = /[^~]+/;
-const emojiListPath = './assets/emoji/';
+const connectionOptions = !!process.env.REDIS_CONNECTION_STRING ? null : {'url': process.env.REDIS_CONNECTION_STRING}
 
 emoticonRouter.use((req, res, next) => {
   const origin = res.get('origin') || 'https://teams.microsoft.com';
@@ -21,12 +18,12 @@ emoticonRouter.post('/emoticon/:emoticon', bodyParser.raw({
 	type: 'image/*'
 }), async (req, res) => {
 	const data: Buffer = req.body;
-	const hexData = data.toString('base64');
+	const hexData = data.toString('hex');
 	const split = req.params.emoticon.split('.')
 	const emojiName = split[0]
 	let emojiExtension = split[1]
 	try {
-		const client = await createClient();
+		const client = await createClient(connectionOptions);
 		const emoji = await client.get(emojiName);
 		if (emoji) {
 			res.status(400).send('emoji already exists');
@@ -42,7 +39,7 @@ emoticonRouter.post('/emoticon/:emoticon', bodyParser.raw({
 emoticonRouter.delete('/emoticon/:emoticon', async (req, res) => {
   const emojiName = req.params.emoticon;
   try {
-    const client = await createClient();
+    const client = await createClient(connectionOptions);
 		client.del(emojiName);
     res.status(200).send('deleted emoji ' + emojiName);
   } catch (err) {
@@ -56,7 +53,7 @@ emoticonRouter.get('/emoticon/:emoticon', async (req, res) => {
 	let extension;
 	let imageData;
   try {
-    const client = await createClient();
+    const client = await createClient(connectionOptions);
     const hash = await client.hgetall(emoticonName);
     if (!hash) {
       res.status(404).send('not found');
@@ -73,12 +70,12 @@ emoticonRouter.get('/emoticon/:emoticon', async (req, res) => {
   res.set('Content-Type', `image/${extension}`);
   const maxAge = Math.round(60 * 60 * 24 * (15 + (Math.random() * 2 - 1)));
   res.header('Cache-Control', `public, max-age=${maxAge}`);
-  res.send(Buffer.from(imageData, 'base64'));
+  res.send(Buffer.from(imageData, 'hex'));
 });
 
 emoticonRouter.get('/emoticons', async (req, res) => {
   try {
-    const client = await createClient();
+    const client = await createClient(connectionOptions);
 		res.send(await client.keys('*'));
   } catch (err) {
     res.status(500).send('Error ' + err);
